56c0885e5de98b3f6f5b14de55a9f7df2e6483fb
IreneCaiazzo <irene.caiazzo707@gmail.com>
2023-07-04 00:05:36 +0200

    add comics index route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComicController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'home'])->name('home');
 Route::get('/about', [PageController::class, 'about'])->name('about');
+
+Route::get('/comics', [ComicController::class, 'index'])->name('comics.index');
